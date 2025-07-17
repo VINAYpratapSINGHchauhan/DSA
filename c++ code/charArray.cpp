5514e7344ef1cb45d6cfb237b836c80fa6d16ef9
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-07-17 12:51:08 +0530

    char array question

--- c++ code/charArray.cpp
+++ c++ code/charArray.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-int findLength (char ch[],int size){
+int findLength(char ch[], int size)
+{
     // int length=strlen(ch);     //this is predefined funciton of calculating length
-   int length=0;
-    while(ch[length]!='\0'){
-    length++;
-   }
-   return length;
+    int length = 0;
+    while (ch[length] != '\0')
+    {
+        length++;
+    }
+    return length;
+}
+void printUpperCase(char ch[], int size)
+{
+    int i = 0;
+    while (ch[i] != '\0')
+    {
+        // check if the character is lower case
+        if (ch[i] >= 'a' && ch[i] <= 'z')
+        {
+            ch[i] = ch[i] - 'a' + 'A';
+        }
+        i++;
+    }
+    cout << "after upper case operation : " << ch << endl;
+}
+void replacewithSpace(char ch[], int size)
+{
+    int i = 0;
+    while (ch[i] != '\0')
+    {
+        if (ch[i] == '@')
+        {
+            ch[i] = ' ';
+        }
+        i++;
+    }
+}
+bool checkPalindrome(char ch[], int length)
+{
+    int i = 0;
+    int j = length - 1;
+    while(i<=j){
+        if(ch[i]!=ch[j]){
+            return false;
+        }
+        i++ , j--;
+    }
+    return true;
 }
 int main()
 {
     // char arrays
     char chArray[100];
     // // input directly
-    cout << "enter char array : ";
-    cin >> chArray;
+    cout << "enter char array we are getting input in cin.getline : ";
+    // cin >> chArray;
+    cin.getline(chArray, 100);
     // // output directly
     // cout << "value of char array : " << chArray<<endl;
 
@@ -41,7 +82,30 @@ int main()
     // cout<<"char array with spaces or tab that are called delimitor : "<<endl;
     // cout<<chArray<<endl;
 
-    // printing the length of the char array
-    int len =findLength(chArray,100);
-    cout<<"length of the char arry is : "<<len<<endl;
+    // // printing the length of the char array
+    int len = findLength(chArray, 100);
+    // cout << "length of the char arry is : " << len << endl;
+
+    // reverse the char array
+    // same code using two pointer approach see in copy
+    // utility function = reverse(chArray,chArray+len);
+
+    // // convert to upper case
+    // printUpperCase(chArray, 100);
+
+    // // replace @ with ' '(space)
+    // replacewithSpace(chArray, 100);
+    // cout << chArray << endl;
+
+    // check palindrome
+    if (checkPalindrome(chArray, len))
+    {
+        cout << "valid palindrome" << endl;
+    }
+    else
+    {
+        cout << "not a valid palindrome" << endl;
+    }
+
+     
 }
