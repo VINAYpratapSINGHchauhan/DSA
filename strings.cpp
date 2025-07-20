e2fc86692eb481f569555904e9aa77e3c710cb84
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-07-20 13:50:47 +0530

    string question

--- strings.cpp
+++ strings.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 string removeDuplicates(string s)
 {
@@ -146,9 +149,109 @@ string decodeTheString(string key, string message)
     return ans;
 }
 
+int findMinimumTimeToCollectGArbage(vector<string> &garbage, vector<int> &travel)
+{
+    int pickTimeP = 0; // 1-2-3-4
+    int pickTimeM = 0; // 1-2-3
+    int pickTimeG = 0; // 1-2-3
+
+    int lastIndexP = 0; // 0-0-1-3
+    int lastIndexM = 0; // 1-2
+    int lastIndexG = 0; // 0-1-2
+
+    int travelTimeP = 0;
+    int travelTimeM = 0;
+    int travelTimeG = 0;
+
+    for (int i = 0; i < garbage.size(); i++)
+    {
+        string str = garbage[i];
+        for (auto ch : str)
+        {
+            if (ch == 'P')
+            {
+                pickTimeP++;
+                lastIndexP = i;
+            }
+            else if (ch == 'G')
+            {
+                pickTimeG++;
+                lastIndexG = i;
+            }
+            else if (ch == 'M')
+            {
+                pickTimeM++;
+                lastIndexM = i;
+            }
+        }
+    }
+
+    for (int i = 0; i < lastIndexP; i++)
+    {
+        travelTimeP += travel[i];
+    }
+    for (int i = 0; i < lastIndexM; i++)
+    {
+        travelTimeM += travel[i];
+    }
+    for (int i = 0; i < lastIndexG; i++)
+    {
+        travelTimeG += travel[i];
+    }
+
+    return (pickTimeP + pickTimeM + pickTimeG) + (travelTimeP + travelTimeM + travelTimeG);
+}
+
+string customSortSorting(string order, string str)
+{
+    unordered_map<char, int> priority;
+    for (int i = 0; i < order.length(); ++i)
+    {
+        priority[order[i]] = i;
+    }
+    sort(str.begin(), str.end(), [&](char a, char b)
+         { return priority[a] < priority[b]; });
+    return str;
+}
+
+string createMapping(string & str)
+{
+    // create mapping 
+    char start = 'a';
+    char map[300] = {0};
+    for (auto ch : str)
+    {
+        if (map[ch] == 0)
+        {
+            map[ch] = start;
+            start++;
+        }
+    }
+    //update the string with mapped value
+    for(int i=0;i<str.length();i++){
+        char ch=str[i];
+        str[i]=map[ch];
+    }
+    return str;
+}
+vector<string> findAndReplacePattern(vector<string> &words, string pattern)
+{   
+    vector<string> ans;
+    createMapping(pattern);
+    for(auto str:words){
+        string originalStr=str;
+        createMapping(str);
+        if(str==pattern){
+            ans.push_back(originalStr);
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     string s1;
+    cout << "enter string s1 : ";
     getline(cin, s1);
     // cout <<"string is : "<<name<<endl;
 
@@ -191,11 +294,24 @@ int main()
     // string result = decodeTheString(key, message);
     // cout << "decoded message is : " << result << endl;
 
-    // question 7 -find minimum amount of time to collect the garbage from the garbage vector string and the travel time indicatign string vector
-    
-    // question 8 (791 in lc) - custom sort sorting , sorting a given string on the basis of order of second string given with 
+    // // question 7 -find minimum amount of time to collect the garbage from the garbage vector string and the travel time indicatign string vector
+    // vector<string> garbage = {"PPG","GMP","MMG","P"};
+    // vector<int> travel = {2, 4, 3};
+    // int result = findMinimumTimeToCollectGArbage(garbage, travel);
+    // cout << "minimum time to collect garbage is : " << result << endl;
 
-    // question 9 (890 in lc) - find and replace pattern ,  given  a list of strings 'words' and a string 'pattern' and  returns a list of strings of words[i] that matches the pattern 
+    // // question 8 (791 in lc) - custom sort sorting , sorting a given string on the basis of order of second string given with
+    // string order = "cba";
+    // string str = "abcd";
+    // string sortedStr = customSortSorting(order, str);
+    // cout << "sorted str string : " << sortedStr << endl;
 
-
-}
+    // // question 9 (890 in lc) - find and replace pattern ,  given  a list of strings 'words' and a string 'pattern' and  returns a list of strings of words[i] that matches the pattern
+    // vector<string> words = {"abc", "deq", "mee", "aqq", "dkd", "ccc"};
+    // string pattern = "abb";
+    // vector<string> result = findAndReplacePattern(words, pattern);
+    // for (auto e : result)
+    // {
+    //     cout << e << " ";
+    // }
+} 
