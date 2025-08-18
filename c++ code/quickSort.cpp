e52ae3b46ac2596648a017add7d84efeeb119277
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-08-18 23:58:15 +0530

    quick sort

---
+++ c++ code/quickSort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <limits.h>
+#include <vector>
+using namespace std;
+int partition(vector<int> &arr, int s, int e)
+{
+    int element = arr[s]; // taking the first element
+    int count = 0;
+    // counting number of element which are smaller than our element
+    for (int i = s+1; i <= e; i++)
+    {
+        if (arr[i] < element)
+        {
+            count++;
+        }
+    }
+    // puting that element at its right place
+    int pivotIndex = s + count;
+    swap(arr[s], arr[pivotIndex]);
+    int pivotElement = arr[pivotIndex];
+    // making left side element smller than our pivot element and rigth greater than our pivot element
+    int i = s;
+    int j = e;
+    while (i<pivotIndex && j>pivotIndex)
+    {
+         if (arr[i] <= pivotElement) {
+            i++;
+        } else if (arr[j] > pivotElement) {
+            j--;
+        } else {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+    return pivotIndex;
+}
+void solve(vector<int> &arr, int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+    // partition
+    int p = partition(arr, s, e);
+    // recursive calls
+    solve(arr, s, p - 1);
+    solve(arr, p+1, e);
+}
+void quickSort(vector<int> &arr)
+{
+    solve(arr, 0, arr.size() - 1);
+    return;
+}
+int main()
+{
+    vector<int> arr = {9 ,2 ,6 ,9 ,1 ,4 ,4 ,3};
+    quickSort(arr);
+    for (int i =0;i<arr.size();i++)
+    {
+        cout <<arr[i] << " ";
+    }
+    cout << endl;
+}
