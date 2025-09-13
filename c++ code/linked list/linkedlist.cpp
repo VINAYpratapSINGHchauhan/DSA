730a69bc98d43e320a4f9f0a95f311d69a14edf9
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-09-13 17:20:50 +0530

    insertion in linked list

--- c++ code/linked list/linkedlist.cpp
+++ c++ code/linked list/linkedlist.cpp
@@ -1,5 +1,115 @@
-#include <iostream>>
+#include <iostream>
 using namespace std;
-int main(){
+class node
+{
+public:
+    int data;
+    node *next;
+    node()
+    {
+        this->next = NULL;
+    }
+    node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+void printLL(node *head)
+{
+    node *temp = head; // point to be noted
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int getLength(node *head)
+{
+    node *temp = head; // point to be noted
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void insertAtBegin(node *&head, int value)
+{
+    node *newnode = new node(value);
+    newnode->next = head;
+    head = newnode;
+}
+void insertAtTail(node *&tail, int value)
+{
+    node *newnode = new node(value);
+    tail->next = newnode;
+    tail = newnode;
+}
+void insertAtPos(node *&head, node *&tail, int value, int pos)
+{
+    int length = getLength(head);
+    cout << "length is : " << length << endl;
+    if (pos < 1 || pos > length+1)
+    {
+        cout << "invalid positon " << endl;
+        return;
+    }
+    if(pos==1){
+        insertAtBegin(head, value);
+        return;
+    }
+    if(pos==length+1){
+        insertAtTail(tail,value);
+        return;
+    }
+       node *newnode = new node(value);
+        // node *prev=NULL;
+        // node *curr=head;
+        node *prev=head;
+        while (pos!=1){
+            // prev=curr;
+            // curr=curr->next;
+            prev=prev->next;
+            pos--;
+        }
+        // prev->next=newnode;
+        // newnode->next=curr;
+        newnode->next=prev->next;
+        prev->next=newnode;
     
+}
+int main()
+{
+    // node creation
+    //  node head; //static allocation
+    node *first = new node(0);
+    node *second = new node(5);
+    node *third = new node(10);
+    node *fourth = new node(15);
+    (first->next) = second;
+    second->next = third;
+    third->next = fourth;
+    // linked list created by normal way
+    node *head = first;
+    node *tail = fourth;
+    // printLL(head);
+    // int len = getLength(head);
+    // cout << "length is : " << len << endl;
+
+    // insertion & deletion in linked list
+
+    // insert at beginiing
+    insertAtBegin(head, 100);
+    printLL(head);
+    // insert an end
+    insertAtTail(tail, 500);
+    printLL(head);
+    // insert at given position
+     
+    insertAtPos(head, tail, 50, 7);
+    printLL(head);
 }
