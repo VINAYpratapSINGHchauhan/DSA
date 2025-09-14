089228321b0f965da55f45e91ea139bbdd668e02
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-09-15 01:22:42 +0530

    questions

---
+++ c++ code/linked list/quesitons.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // reverse a list - LC: 206
+    // find middle of the list - LC: 876
+    // check palindrome linked list - LC: 234
+    // check cycle in a Linked List - LC: 141
+}
