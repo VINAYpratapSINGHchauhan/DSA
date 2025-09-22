e8f0b5e15c61c7cc06457ed06c0aa1144e2d1e4a
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-09-22 10:50:54 +0530

    questions on stack part 1

--- c++ code/stack/question.cpp
+++ c++ code/stack/question.cpp
@@ -33,17 +33,121 @@ int findMidElement(stack<int> st)
     int ans = solve(st, midPos);
     return ans;
 }
+
+void insertAtBottom(stack<int> &st, int element)
+{
+    if (st.empty())
+    {
+        st.push(element);
+        // cout << "element inserted successfully " << endl;
+        return;
+    }
+    else
+    {
+        int temp = st.top();
+        st.pop();
+        insertAtBottom(st, element);
+        st.push(temp);
+    }
+}
+void reverseStack(stack<int>&st){
+    if(st.empty()){
+        // cout<<"stack revresed successfully "<<endl;
+        return;
+    }
+    int temp=st.top();
+    st.pop();
+    reverseStack(st);
+    insertAtBottom(st,temp);
+}
+void insertInSorted(stack<int>&st,int e){
+    if(st.empty()||e>st.top()){
+        st.push(e);
+        return;
+    }
+    int temp=st.top();
+    st.pop();
+    insertInSorted(st,e);
+    st.push(temp);
+}
+void sortTheStack(stack<int> & st){
+    if(st.empty()){
+        return;
+    }
+    int temp=st.top();
+    st.pop();
+    sortTheStack(st);
+    insertInSorted(st,temp);
+}
 int main()
 {
-    // 1. find middle of the stack
     stack<int> st;
-    // st.push(70);
-    // st.push(60);
-    // st.push(50);
-    // st.push(40);
-    // st.push(30);
-    // st.push(20);
-    // st.push(10);
+    st.push(70);
+    st.push(60);
+    st.push(50);
+    st.push(40);
+    st.push(30);
+    st.push(20);
+    st.push(10);
 
-    cout << "middle element is: " << findMidElement(st) << endl;
+    // 1. find middle of the stack
+    // cout << "middle element is: " << findMidElement(st) << endl;
+
+    // 2. insert at the bottom of the stack
+    // int elementToInsertAtLast = 590;
+    // insertAtBottom(st, elementToInsertAtLast);
+    // while ((!st.empty()))
+    // {
+    //     cout << st.top() << " ";
+    //     st.pop();
+    // }
+    // cout << endl;
+
+    // 3. REVERSE THE STACK
+    // cout << "before reversal : " << endl;
+    // while ((!st.empty()))
+    // {
+    //     cout << st.top() << " ";
+    //     st.pop();
+    // }
+    // cout << endl;
+    // reverseStack(st); //reverse the stack
+    // cout << "after reversal : " << endl;
+    // while ((!st.empty()))
+    // {
+    //     cout << st.top() << " ";
+    //     st.pop();
+    // }
+    // cout << endl;
+
+    // 4. INSERT ELEMENT AT ITS APPROPRIATE PLACE IN SORTED STACK
+    //creating sorted stack
+    stack <int> sortedST;
+    sortedST.push(10);
+    sortedST.push(20);
+    sortedST.push(30);
+    sortedST.push(40);
+    // insertInSorted(sortedST,110);
+    // while ((!sortedST.empty()))
+    // {
+    //     cout << sortedST.top() << " ";
+    //     sortedST.pop();
+    // }
+    // cout << endl;
+
+    // 5. SORT THE UNSORTED STACK
+    stack<int> unsorted;
+    unsorted.push(52);
+    unsorted.push(101);
+    unsorted.push(2);
+    unsorted.push(9);
+    unsorted.push(11);
+    sortTheStack(unsorted);
+     while ((!unsorted.empty()))
+    {
+        cout << unsorted.top() << " ";
+        unsorted.pop();
+    }
+    cout << endl;
+
 }
