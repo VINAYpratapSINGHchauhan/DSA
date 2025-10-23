8910659a80e55aa01920f47fd3256974b62c119c
VINAYpratapSINGHchauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-10-23 17:36:15 +0530

    Dequeue

--- c++ code/queue/queue.cpp
+++ c++ code/queue/queue.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <deque>
 using namespace std;
 class que
 {
@@ -180,7 +181,7 @@ public:
     }
     void display()
     {
-        cout << "printing queue : " ;
+        cout << "printing queue : ";
         for (int i = 0; i < size; i++)
         {
             cout << arr[i] << " ";
@@ -188,6 +189,265 @@ public:
         cout << endl;
     }
 };
+class deq
+{
+    // normal doubly ended queue
+public:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+    deq(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        this->front = -1;
+        this->rear = -1;
+    }
+    void pushback(int val)
+    {
+        // overflow
+        // emptycase
+        // normal flow
+        if (rear = size - 1)
+        {
+            cout << "overflow: queue is full" << endl;
+        }
+        else if (front == -1 && rear == -1)
+        {
+            front++;
+            rear++;
+            arr[rear] = val;
+        }
+        else
+        {
+            rear++;
+            arr[rear] = val;
+        }
+    }
+    void pushfront(int val)
+    {
+        // overflow
+        // emptycase
+        // normal flow
+        if (front ==0)
+        {
+            cout << "overflow: queue is full" << endl;
+        }
+        else if (front == -1 && rear == -1)
+        {
+            front++;
+            rear++;
+            arr[rear] = val;
+        }
+        else
+        {
+            front--;
+            arr[front] = val;
+        }
+    }
+    void popback()
+    {
+        //underflow
+        //sigle element
+        //normal
+        if (front == -1 && rear == -1)
+        {
+            // queue is empty
+            cout << "underflow: queue is empty" << endl;
+        }
+        else
+        {
+            if (front == rear)
+            {
+                // single element in queue
+                arr[front] = -1;
+                front = rear = -1;
+                return;
+            }
+            else
+            {
+                // normal popping
+                arr[rear] = -1;
+                rear--;
+                return;
+            }
+        }
+        
+    }
+    void popfront()
+    {
+        //underflow
+        //sigle element
+        //normal
+        if (front == -1 && rear == -1)
+        {
+            // queue is empty
+            cout << "underflow: queue is empty" << endl;
+        }
+        else
+        {
+            if (front == rear)
+            {
+                // single element in queue
+                arr[front] = -1;
+                front = rear = -1;
+                return;
+            }
+            else
+            {
+                // normal popping
+                arr[front] = -1;
+                front++;
+                return;
+            }
+        }
+    }
+};
+
+//circular dequeue 
+class CircularDeq
+{
+    // normal doubly ended queue
+public:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+    CircularDeq(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        this->front = -1;
+        this->rear = -1;
+    }
+    void pushback(int val)
+    {
+        // overflow
+        // emptycase
+        // normal flow
+        // case 1: jisme overflow ho rha ho
+        if (front == 0 && rear == size - 1 || front == rear + 1)
+        {
+            cout << "OVERFLOW" << endl;
+            return;
+        }
+        else if (front == -1 && rear == -1)
+        {
+            // case 2: first element insert kr rhe h hm
+            front++;
+            rear++;
+            arr[rear] = val;
+        }
+        else if (rear == size - 1 && front != 0)
+        {
+            // case 3 : circular case
+            rear = 0;
+            arr[rear] = val;
+        }
+        else
+        {
+            // normal pushing
+            rear++;
+            arr[rear] = val;
+        }
+    }
+    void pushfront(int val)
+    {
+        // overflow
+        // emptycase
+        // normal flow
+        // case 1: jisme overflow ho rha ho
+        if (front == 0 && rear == size - 1 || front == rear + 1)
+        {
+            cout << "OVERFLOW" << endl;
+            return;
+        }
+        else if (front == -1 && rear == -1)
+        {
+            // case 2: first element insert kr rhe h hm
+            front++;
+            rear++;
+            arr[front] = val;
+        }
+        else if (front==0&&rear!=size-1)
+        {
+            // case 3 : circular case
+            front = size-1;
+            arr[front] = val;
+        }
+        else
+        {
+            // normal pushing
+            front--;
+            arr[front] = val;
+        }
+    }
+    void popback()
+    {
+        //underflow
+        //sigle element
+        //normal
+        // case 1: popping from empty queue
+        if (front == -1 && rear == -1)
+        {
+            cout << "UNDERFLOW" << endl;
+            return;
+        }
+        else if (front == rear)
+        {
+            // case 2: popping single element
+            arr[front] = -1;
+            front = -1;
+            rear = -1;
+        }
+        else if (rear == 0)
+        {
+            // case 3 : circular wala
+            arr[rear] = 0;
+            rear = size-1;
+        }
+        else
+        {
+            // normal popping
+            arr[rear] = 0;
+            rear--;
+        }
+        
+    }
+    void popfront()
+    {
+        //underflow
+        //sigle element
+        //normal
+        // case 1: popping from empty queue
+        if (front == -1 && rear == -1)
+        {
+            cout << "UNDERFLOW" << endl;
+            return;
+        }
+        else if (front == rear)
+        {
+            // case 2: popping single element
+            arr[front] = -1;
+            front = -1;
+            rear = -1;
+        }
+        else if (front == size - 1)
+        {
+            // case 3 : circular wala
+            arr[front] = 0;
+            front = 0;
+        }
+        else
+        {
+            // normal popping
+            arr[front] = 0;
+            front++;
+        }
+    }
+};
+
 int main()
 {
 
@@ -253,32 +513,49 @@ int main()
     // cout<<q.getSize();
     // end of linear queue
 
-    // CIRCULAR QUEUE
-    // here the size of the queue is rear-front +1 and if space available anywhere in the array it will be used no memory wastage i.e circular queue explanation in notebook
-    Cque cq1(5);
-    cq1.display();
-    cq1.push(10);
-    cq1.push(20);
-    cq1.push(30);
-    cq1.display();
-    cq1.push(40);
-    cq1.push(50);
-    cq1.display();
-    cq1.push(60);
-    cq1.display();
-    cq1.pop();
-    cq1.pop();
-    cq1.pop();
-    cq1.display();
-    cq1.push(60);
-    cq1.push(70);
-    cq1.display();
-    cq1.push(80);
-    cq1.display();
-    cq1.push(90);
-    cq1.display();
-    cq1.pop();
-    cq1.pop();
-    cq1.pop();
-    cq1.display();
+    // // CIRCULAR QUEUE
+    // // here the size of the queue is rear-front +1 and if space available anywhere in the array it will be used no memory wastage i.e circular queue explanation in notebook
+    // Cque cq1(5);
+    // cq1.display();
+    // cq1.push(10);
+    // cq1.push(20);
+    // cq1.push(30);
+    // cq1.display();
+    // cq1.push(40);
+    // cq1.push(50);
+    // cq1.display();
+    // cq1.push(60);
+    // cq1.display();
+    // cq1.pop();
+    // cq1.pop();
+    // cq1.pop();
+    // cq1.display();
+    // cq1.push(60);
+    // cq1.push(70);
+    // cq1.display();
+    // cq1.push(80);
+    // cq1.display();
+    // cq1.push(90);
+    // cq1.display();
+    // cq1.pop();
+    // cq1.pop();
+    // cq1.pop();
+    // cq1.display();
+
+    // doubly ended queue
+    deque<int> dq;
+    dq.push_back(10);
+    cout << dq.back() << endl;
+    dq.push_back(20);
+    cout << dq.front() << " ";
+    cout << dq.back() << endl;
+    dq.push_front(00);
+    cout << dq.front() << " ";
+    cout << dq.back() << endl;
+    cout << dq.size() << endl;
+    dq.pop_back();
+    dq.pop_front();
+    cout << dq.back() << endl;
+    cout << dq.size() << endl;
+    // implementing deque from scratch
 }
