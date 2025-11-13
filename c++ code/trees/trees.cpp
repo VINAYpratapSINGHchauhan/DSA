d8c858dad4a4f2c84dacad2e8239928ee865022e
VINAYpratapSINGHchauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-11-13 19:35:13 +0530

    level order traversel with and without marker

--- c++ code/trees/trees.cpp
+++ c++ code/trees/trees.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 class node{
     public:
@@ -71,6 +72,42 @@ void postOrderTraversel(node* root){
     // 3. N- cuurent node
     cout<<root->data<<" ";
 }
+void levelOrderTraversel(node*root){ //this is without marker that is here all node are in one line
+    // row wise traversel that is breadth first traversel
+    queue<node*>q;
+    q.push(root);
+    //traversel
+    while(!q.empty()){
+        //front ko print kro aur pop kro 
+        node*front =q.front();
+        q.pop();
+        cout<<front->data<<" ";
+        //left aur right child dalo queue mei 
+        if(front->left!=NULL)q.push(front->left);
+        if(front->right!=NULL)q.push(front->right);
+    }
+}
+void levelOrderTraverselWithMarker(node*root){ //this is with marker that each level element in each line
+    // row wise traversel that is breadth first traversel
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    //traversel
+    while(!q.empty()&&q.size()>1){
+        //front ko print kro aur pop kro 
+        node*front =q.front();
+        q.pop();
+        if(front==NULL){
+            cout<<endl;
+            q.push(NULL);
+        }else{
+            cout<<front->data<<" ";
+            //left aur right child dalo queue mei 
+            if(front->left!=NULL)q.push(front->left);
+            if(front->right!=NULL)q.push(front->right);
+        }
+    }
+}
 int main(){
     node* root=createTree();
     cout<<"Pre Order Traversel of the Tree is "<<endl;
@@ -81,5 +118,11 @@ int main(){
     cout<<endl;
     cout<<"Post Order Traversel of the Tree is "<<endl;
     postOrderTraversel(root);
+    cout<<endl;
+    cout<<"level Order Traversel of the Tree is "<<endl;
+    levelOrderTraversel(root);
+    cout<<endl;
+    cout<<"level Order Traversel with marker of the Tree is "<<endl;
+    levelOrderTraverselWithMarker(root);
     return 0;
 }
