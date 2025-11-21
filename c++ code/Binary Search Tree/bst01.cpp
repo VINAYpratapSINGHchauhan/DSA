1cee9c3ab9e710840fda2a1fd9f346ddfd2f4375
VINAYpratapSINGHchauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-11-21 15:20:25 +0530

    creation of binary search tree

---
+++ c++ code/Binary Search Tree/bst01.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class TreeNode{
+    public:
+    int data;
+    TreeNode*left;
+    TreeNode*right;
+    TreeNode(int val){
+        this->data=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+TreeNode* createBSTnode(TreeNode*root,int data){
+    if(root==NULL)return new TreeNode(data);
+    if(data>root->data)root->right=createBSTnode(root->right,data);
+    if(data<root->data)root->left=createBSTnode(root->left,data);
+    return root;
+}
+void createBST(TreeNode*&root){
+    cout<<"enter the value of node"<<endl;
+    int data;
+    cin>>data;
+    while(data!=-1){
+        root=createBSTnode(root,data);
+        cin>>data;
+    }
+}
+void levelOrderTraverselWithMarker(TreeNode*root){ //this is with marker that each level element in each line
+    // row wise traversel that is breadth first traversel
+    if(root==NULL)return ;
+    queue<TreeNode*>q;
+    q.push(root);
+    q.push(NULL);
+    //traversel
+    while(!q.empty()){
+        //front ko print kro aur pop kro 
+        TreeNode*front =q.front();
+        q.pop();
+        if(front==NULL){
+            cout<<endl;
+            if(!q.empty()){
+            q.push(NULL);
+            }
+        }else{
+            cout<<front->data<<" ";
+            //left aur right child dalo queue mei 
+            if(front->left!=NULL)q.push(front->left);
+            if(front->right!=NULL)q.push(front->right);
+        }
+    }
+}
+int main(){
+    // creating a binary search tree
+    TreeNode*root=NULL;
+    createBST(root);
+    cout<<"printing binary search tree"<<endl;
+    levelOrderTraverselWithMarker(root);
+    return 0;
+}
