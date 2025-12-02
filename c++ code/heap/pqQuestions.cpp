401c3ba8d577ecbc946ae732eeae9879211699b1
VINAYpratapSINGHchauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-12-02 21:18:13 +0530

    pq questions

---
+++ c++ code/heap/pqQuestions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //question 1 - merge k sorted arrays (gfg)
+    //question 2 - merge k sorted lists (lc)
+    //question 3 - smallest range in k lists (lc)
+    //question 4 - remove stones to minimize the total (lc)
+    //question 5 - reorganise string (lc)
+    //question 6 - longest happy string (lc)
+    //question 7 - find median from data stream (lc)
+    return 0;
+}
