55c441e87bc97fdedab9eea076c0dabea282edd1
VINAYpratapSINGHchauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2026-01-10 22:35:15 +0530

    Create dp01.cpp

---
+++ c++ code/DP/dp01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // QUESTIONS FOR 1-D DP
+    //ques fibonaaci series
+    //ques house robbber;
+    //ques coin chnge
+    //ques painting fence
+    //ques frog jump gfg
+    //ques frog jump lc
+    //ques climb stairs
+
+
+    return 0;
+}
